f6de83dc71c8bf77bf34ab5621c17fef5ca837fe
José Alfredo Rivera Turcios <jose.alfredo.rivera@gmail.com>
2021-02-18 00:36:39 -0800

    Create ig-grabber-config.php
    Adding sample config

---
+++ ig-grabber-config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+  'DB_NAME' => 'DATABASE NAME',
+  'DB_USER' => 'DATABASE USER',
+  'DB_PASSWORD' => 'DATABASE PASSWORD',
+  'DB_HOST' => 'localhost',
+  'DB_TABLE' => 'DATABASE TABLE'
+];
+?>
